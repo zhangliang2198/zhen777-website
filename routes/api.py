50c8d06ad16434c44e4015bf88e4fefa241f3431
mikoozhang <48906088@qq.com>
2023-03-15 18:19:07 +0800

    init

--- routes/api.py
+++ routes/api.py
@@ -6,7 +6,5 @@ from app.http.api import users
 api_router = APIRouter()
 
 api_router.include_router(demo.router, tags=["demo"])
-
 api_router.include_router(auth.router, tags=["auth"])
-
 api_router.include_router(users.router, tags=["users"])
